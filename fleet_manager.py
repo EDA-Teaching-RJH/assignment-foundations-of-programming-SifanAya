9c7e86f9f7c4cea42b1a0d5e32eeea18fde170fd
SifanAya <ska1@kent.ac.uk>
2026-02-16 04:43:43 +0000

    Feature 2

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,3 +4,17 @@ def init_database():
     divs = ["Command", "Command", "Operations", "Operations", "Sciences"]
     ids = ["1", "2", "3", "4", "5"]
     return names, ranks, divs, ids
+
+def display_menu(student_name):
+    print("\n--- STAR FLEET MANAGER ---")
+    print("Logged in as student: " + student_name)
+    print("1. View Roster")
+    print("2. Add Member")
+    print("3. Remove Member")
+    print("4. Update Rank")
+    print("5. Search Crew")
+    print("6. Filter by Division")
+    print("7. Calculate Payroll")
+    print("8. Count Officers")
+    print("9. Exit")
+    
