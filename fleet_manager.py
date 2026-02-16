bf7863c6061f73988dcd346936623331dc00a06d
SifanAya <ska1@kent.ac.uk>
2026-02-16 04:36:50 +0000

    Feature 1

---
+++ fleet_manager.py
@@ -0,0 +1,6 @@
+def init_database():
+    names = ["Jean-Luc Picard", "William Riker", "Data", "Geordi La Forge", "Deanna Troi"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lt. Commander", "Commander"]
+    divs = ["Command", "Command", "Operations", "Operations", "Sciences"]
+    ids = ["1", "2", "3", "4", "5"]
+    return names, ranks, divs, ids
